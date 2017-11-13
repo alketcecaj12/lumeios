4d02cd24d37aaba13b17ac4a535c1f5d232c27c0
datatreemap <[data2maping@gmail.com]>
2017-11-13 15:58:21 +0100

    iosmobile4lume

---
+++ js/my-app.js
@@ -0,0 +1,16 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+
+
